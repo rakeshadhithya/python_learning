f16e3b8142556a949b550fd60302844ebb2aa009
Rakesh Adhithya <rakkeshadithya00@gmail.com>
2026-08-12 09:26:05 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,21 @@
+#variable
+name = 'Rakesh'
+#multiple assignment
+a, b, c = 1, 2, 3 
+#same value 
+a = b = c = 0 
+#reassignemnt 
+a = 10
+a = 21
+#swapping
+a = 10 
+b = 20
+a,b = b, a 
+#delete
+del a 
+#single line comment
+'''
+multi 
+   line 
+comment
+'''
